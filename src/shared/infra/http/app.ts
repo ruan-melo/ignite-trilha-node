8b6c6f561e991822d95da58b2c0eb6605e7477d8
Ruan Tenório de Melo <ruantmelo@gmail.com>
2022-04-01 17:23:06 -0300

    config cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,6 +4,7 @@ import "dotenv/config";
 import "../../container";
 import "../../container/providers";
 
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUI from "swagger-ui-express";
@@ -25,6 +26,7 @@ app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(routes);
 app.get("/", (req, res) => res.json({ message: "Hello World" }));
 
